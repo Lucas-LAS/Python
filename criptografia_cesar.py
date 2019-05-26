efbd0228c90ddfae88217292d25be2edd970670e
Lucas Silva <lucas.azevedo.silva@live.com>
2019-05-26 18:31:17 -0300

    Criptografia e Descriptografia funcional aceitado espaço vazio e ponto "."

--- criptografia_cesar.py
+++ criptografia_cesar.py
@@ -1,37 +1,40 @@
 # --*-- encoding UTF-8 --*--
 
 print ("\n Cifra de César")
-print ("\n lembrando que os dados inseridos não pode conter acentuação grafica ou mesmo caracteres especiais ")
+print ("\n lembrando o unico caractere especial aceito é o pont ""."" " )
 entrada = input("\n Entre com o valor a ser criptografado:")
 
 dados = entrada.lower()
 dicio = "abcdefghijklmnopqrstuvwxyz" 
 logica_de_césar = 3
+
 print (dados)
+
 criptografada = ""
 for i in dados:
-    
-    if (i != " "):
-     dicionario = dicio.index(i)
-     criptografada += dicio[(dicionario+logica_de_césar) % 26 ]
-    else:
-            (i==" ")
-            criptografada += i
+        if ((i != " ") and (i != ".")):
+           dicionario = dicio.index(i)
+           criptografada += dicio[(dicionario+logica_de_césar) % len(dicio) ] # módulo para realizar um giro na lista 
+        else:
+                ((i == " ") or (i == "."))
+                criptografada += i
 
 print(criptografada)
 
-"""
+
 Descriptando = ""
 for i in criptografada:
-
-    dicionario = dicio.index(i)
-    Descriptando += dicio[dicionario-logica_de_césar]
-
+        if ((i != " ") and (i != ".")):
+           dicionario = dicio.index(i)
+           Descriptando += dicio[(dicionario-logica_de_césar)% len(dicio)]
+        else:
+              ((i == " ") or (i == "."))
+              Descriptando +=i
 print(Descriptando)
 
 
 
-"""
+
 
 
 
