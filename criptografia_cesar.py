38b01c281874d759d9a7fe8ab9c8eeafa265b577
Lucas Silva <lucas.azevedo.silva@live.com>
2019-06-09 18:05:58 -0300

    Programa funcional (mas pouco bagunçado)

--- criptografia_cesar.py
+++ criptografia_cesar.py
@@ -1,10 +1,12 @@
 # --*-- encoding UTF-8 --*--
+
+from requests_toolbelt import MultipartEncoder
 import requests
 import json 
+import hashlib
 
-#print ("\n Cifra de César")
-#print ("\n lembrando o unico caractere especial aceito é o pont ""."" " )
-#entrada = input("\n Entre com o valor a ser criptografado:")
+
+
 
 
 res = requests.get('https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=8b3cbad8da68e428248f38a3fd0ed75a678006ae') #com requests.get conseguimos acessar o endereço passado 
@@ -19,7 +21,7 @@ arquivo.write(valorjason)
 arquivo = open('answer.json','r')
 
 entrada = json.load(arquivo)
-    #entrada = json.loads(valorjason)
+    
 print ("Só para sabe se o campo certo "+entrada["cifrado"])
    
 
@@ -27,27 +29,37 @@ dados = entrada["cifrado"].lower()
 dicio = "abcdefghijklmnopqrstuvwxyz" 
 logica_de_césar = 8
 
-criptografada = ""
+Descriptando= ""
 for i in dados:
         if (i.isalpha() == True) :# isalpha verifica se letra ou não se for letra o valor passado e true (verdadeiro ) para caracteres especial, número e espaço o valor passado e false(falso)
            dicionario = dicio.index(i)
-           criptografada += dicio[(dicionario-logica_de_césar) % len(dicio) ] # módulo para realizar um giro na lista 
+           Descriptando += dicio[(dicionario-logica_de_césar) % len(dicio) ] # módulo para realizar um giro na lista 
         else:
-               criptografada += i
+               Descriptando+= i
 
-print(criptografada)
-
-
-Descriptando = ""
-for i in criptografada:
-        if (i.isalpha() == True) :
-           dicionario = dicio.index(i)
-           Descriptando += dicio[(dicionario+logica_de_césar)% len(dicio)]
-        else:
-              Descriptando +=i
 print(Descriptando)
 
+entrada["decifrado"] = Descriptando 
+varsha1 = hashlib.sha1(entrada['decifrado'].encode()).hexdigest()
+entrada["resumo_criptografico"] = varsha1
+print (entrada)
+
+criptografadar= ""
+for i in Descriptando:
+        if (i.isalpha() == True) :
+           dicionario = dicio.index(i)
+           criptografadar += dicio[(dicionario+logica_de_césar)% len(dicio)]
+        else:
+              criptografadar +=i
+print(criptografadar)
+
+with  open('answer.json','w') as f:
+  json.dump(entrada,f)
 
 
 
-
+m = MultipartEncoder(fields={'answer': ('file', open('answer.json', 'rb'), 'application/json')})    
+r = requests.post('https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=8b3cbad8da68e428248f38a3fd0ed75a678006ae', data=m,
+                  headers={'Content-Type': m.content_type })
+print (r.status_code)
+print (r.text)
